fe639a37154dd6974f78acd1caf055538d1a7f7b
Rob-n-Dude <m.r.stickcw@yandex.ru>
2021-02-26 20:42:49 +0300

    fix: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+function isLetter(str)
+{
+    return str.toLowerCase() != str.toUpperCase()
+}
+
+module.exports = function createDreamTeam(members) {
+  letters = []
+  try{
+    for(all of members){
+      if(typeof(all)==="string" && isLetter(all.trim()[0])){
+          letters.push(all.trim()[0].toUpperCase())
+      }   
+  
+    }
+  }
+  catch(e){
+    return false
+  }
+  
+  if (letters.length == 0){
+    return false
+  }
+  letters.sort()
+  result = ""
+  for(lett of letters){
+      result += lett
+  }
+  return result
+}
+
+

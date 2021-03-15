45e18995cf74bea7544c1b94c56f61f76428600d
Rob-n-Dude <m.r.stickcw@yandex.ru>
2021-03-15 22:00:16 +0300

    fix: simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,97 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+
+  "link" : [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
+    const chainLength = this.link.filter( (x) => x !== undefined);
+      return chainLength.length;
+    },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.link.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const clearChain = this.link.filter((x) => x !== undefined);
+
+    this.link = clearChain;
+ 
+    if(!Number.isInteger(position) || position <= 0 || position > this.getLength())
+    {
+      this.link = [];
+      throw new Error();
+    }
+    
+      this.link[position-1] = undefined;
+      return this;
+    
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.link = this.link.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const resChain = this.link.filter( (x) => x !== undefined);
+   
+   let result = ""
+   resChain.forEach( function(x)  {
+     result += "( " + x + " )~~"
+   })
+   this.link = [];
+   return result.slice(0,-2);
   }
 };
 
 module.exports = chainMaker;
+
+// const CustomError = require("../extensions/custom-error");
+
+// const chainMaker = {
+//   "link": [],
+
+//   getLength() {
+//     const chainLength = this.link.filter(function(x) {
+//       return x !== undefined;
+//     });
+//     return chainLength.length;
+//   },
+//   addLink(value) {
+//     this.link.push(value);
+//     return this;
+//   },
+//   removeLink(position) {
+//     const clearChain = this.link.filter(function(x) {
+//       return x !== undefined;
+//     });
+//     this.link = clearChain;
+
+//     if(!Number.isInteger(position) ||
+//       position <= 0 ||
+//       position > this.link.length) {
+//         this.link = [];
+//         throw new Error();
+//     }
+//     this.link[position - 1] = undefined;
+//     return this;
+//   },
+//   reverseChain() {
+//     this.link = this.link.reverse();
+//     return this;
+//   },
+//   finishChain() {
+//     const clearChain = this.link.filter(function(x) {
+//       return x !== undefined;
+//     });
+
+//     let result = "";
+//     clearChain.forEach(function (element) {
+//       result += "( " + element + " )~~";
+//     });
+//     chainMaker.link = [];
+//     return result.slice(0,-2);
+//   }
+// };
+
+// module.exports = chainMaker;
